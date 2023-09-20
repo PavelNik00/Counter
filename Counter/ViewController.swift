f8f751aa5502bde3a6a730d64ced28fdcfd96238
PavelNik00 <gh9.111@yandex.ru>
2023-09-20 16:51:24 +0200

    Добавлено во ViewController два лейбла и кнопка подсчета plusAction с методом для создания счетчика по простому заданию

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelText: UILabel!
+    @IBOutlet weak var label: UILabel!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func plusAction(_ sender: Any) {
+        count = count + 1
+        label.text = "\(count)"
+        print("Значение счётчика: \(count)")
+    }
+    
 }
 
