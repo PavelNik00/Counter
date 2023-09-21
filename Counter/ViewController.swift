eb4d012a32fd46327a822698088b3eca21cf6634
PavelNik00 <gh9.111@yandex.ru>
2023-09-21 17:20:32 +0200

    Исправлены по рекомендациям. Добавлены модификаторы доступа private, убраны комментарии, добавлены отступы, пройдена проверка на правописание, обновлен порядок кода.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,16 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var labelText: UILabel!
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet private weak var labelText: UILabel!
+    @IBOutlet private weak var label: UILabel!
     
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var nullButton: UIButton!
+    @IBOutlet private weak var minusButton: UIButton!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var nullButton: UIButton!
     
-    var count = 0
-
-    @IBAction func plusAction(_ sender: Any) {
+    @IBOutlet private weak var textView: UITextView!
+    
+    private var count = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        plusButton.backgroundColor = .systemRed
+        minusButton.backgroundColor = .systemBlue
+        nullButton.backgroundColor = .systemGray
+        label.textColor = .systemBlue
+        labelText.textColor = .black
+        
+        textView.text = "История изменений:"
+        textView.font = UIFont.systemFont(ofSize: 18)
+    }
+    
+    @IBAction private func plusAction(_ sender: Any) {
         if count >= 0 {
             count = count + 1
         } else {
@@ -30,7 +44,7 @@ class ViewController: UIViewController {
         print("Значение счётчика: \(count)")
     }
     
-    @IBAction func minusAction(_ sender: Any) {
+    @IBAction private func minusAction(_ sender: Any) {
         if count >= 1 {
             count = count - 1
             textView.text = textView.text + "\n[\(Date().displayFormat)]: значение изменено на -1"
@@ -43,27 +57,12 @@ class ViewController: UIViewController {
         print("Значение счётчика: \(count)")
     }
     
-    @IBAction func nullAction(_ sender: Any) {
+    @IBAction private func nullAction(_ sender: Any) {
         count = 0
         label.text = "\(count)"
         label.textColor = .systemBlue
         textView.text = textView.text + "\n[\(Date().displayFormat)]: значение сброшено"
         print("Значение счётчика: \(count)")
-    }
-    
-    @IBOutlet weak var textView: UITextView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        plusButton.backgroundColor = .systemRed
-        minusButton.backgroundColor = .systemBlue
-        nullButton.backgroundColor = .systemGray
-        label.textColor = .systemBlue
-        labelText.textColor = .black
-        
-        textView.text = "История изменений:"
-        textView.font = UIFont.systemFont(ofSize: 18)
     }
 }
 
